00c0fe2a246e19259e833feeef07b552b21eb6c2
chetan3clogic <cpandey@3clogic.com>
2020-08-24 13:58:44 +0530

    changes for empty appUUID

--- eventsocket/eventsocket.go
+++ eventsocket/eventsocket.go
@@ -442,20 +442,11 @@ func (h *Connection) Execute(appName, appArg string, lock bool) (*Event, error) 
 // ExecuteUUID is similar to Execute, but takes a UUID and no lock. Suitable
 // for use on inbound event socket connections (acting as client).
 func (h *Connection) ExecuteUUID(uuid, appName, appArg, appUUID string) (*Event, error) {
-	
-	if appUUID != ""{
-		return h.SendMsg(MSG{
-			"call-command":     "execute",
-			"execute-app-name": appName,
-			"execute-app-arg":  appArg,
-			"event-uuid":       appUUID,
-		}, uuid, "")
-	}
-	
 	return h.SendMsg(MSG{
 		"call-command":     "execute",
 		"execute-app-name": appName,
 		"execute-app-arg":  appArg,
+		"event-uuid":       appUUID,
 	}, uuid, "")
 }
 
